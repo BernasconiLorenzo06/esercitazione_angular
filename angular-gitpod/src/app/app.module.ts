1d0d1eaf70b08df3254ba0379d8bd997b6ef40b5
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-11-27 10:46:47 +0000

    secondo commit

--- angular-gitpod/src/app/app.module.ts
+++ angular-gitpod/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CiaoComponent } from './ciao/ciao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CiaoComponent
   ],
   imports: [
     BrowserModule
